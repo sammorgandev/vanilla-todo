18932af4f031d8d2be96eb3dd34f5611a734837b
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-23 23:29:47 -0500

    delete and complete functionality

--- index.js
+++ index.js
@@ -1,55 +1,82 @@
+let todosArray = [];
+
 //query selectors
 const todoList = document.querySelector(".todo-list");
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
+const buttonDiv = document.querySelector(".btn-div");
+const newTodo = document.querySelector(".todo-item");
+const completedButton = document.querySelector(".complete-btn");
 
 //event listeners
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCheck);
 
 //functions
 function addTodo(event) {
-	console.log("addTodo function called");
 	event.preventDefault();
-	//create main div & classes
-	const todoDiv = document.createElement("div");
-	todoDiv.classList.add("todo");
-	todoDiv.classList.add("flex-between");
 
-	//create todo li & classes
-	const newTodo = document.createElement("li");
-	newTodo.innerText = todoInput.value;
-	newTodo.classList.add("todo-item");
+	todosArray.push({
+		id: todosArray.length,
+		text: todoInput.value,
+		completed: false,
+	});
 
-	//append li to main div
-	todoDiv.appendChild(newTodo);
-
-	//create button div
-	const buttonDiv = document.createElement("div");
-	buttonDiv.classList.add("flex");
-	todoDiv.appendChild(buttonDiv);
-
-	const completedButton = document.createElement("button");
-	completedButton.innerHTML = '<i class="fas fa-check"></i>';
-	completedButton.classList.add("complete-btn");
-	buttonDiv.appendChild(completedButton);
-
-	const trashButton = document.createElement("button");
-	trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-	trashButton.classList.add("trash-btn");
-	buttonDiv.appendChild(trashButton);
-	todoList.appendChild(todoDiv);
 	todoInput.value = "";
+
+	console.log(todosArray);
+
+	if (todosArray.length > 0) {
+		todoList.innerHTML = todosArray.map((todo) => {
+			const completedClass = todo.completed ? "strikethrough" : "";
+			return `
+            <div class="todo flex-between">
+            <li class="todo-item ${completedClass}">${todo.text}</li>
+            <div class="flex btn-div">
+                <button type="submit" class="complete-btn" onclick="completeTodo(${todo.id})"><i class="fas fa-check"></i></button>
+                <button type="submit" class="trash-btn" onclick="deleteTodo(${todo.id})"><i class="fas fa-trash"></i></button>
+            </div>
+        </div>`;
+		});
+	}
 }
 
-function deleteCheck(event) {
-	const item = event.target;
-	if (item.classList[0] === "trash-btn") {
-		const todo = item.parentElement;
-		todo.remove();
+function deleteTodo(id) {
+	console.log("delete clicked", id);
+
+	todosArray = todosArray.filter((todo) => todo.id !== id);
+	console.log(todosArray);
+
+	if (todosArray.length > 0) {
+		todoList.innerHTML = todosArray.map((todo) => {
+			const completedClass = todo.completed ? "strikethrough" : "";
+
+			return `
+            <div class="todo flex-between">
+            <li class="todo-item ${completedClass}">${todo.text}</li>
+            <div class="flex btn-div">
+                <button type="submit" class="complete-btn" onclick="completeTodo(${todo.id})"><i class="fas fa-check"></i></button>
+                <button type="submit" class="trash-btn" onclick="deleteTodo(${todo.id})"><i class="fas fa-trash"></i></button>
+            </div>
+        </div>`;
+		});
 	}
-	if (item.classList[0] === "complete-btn") {
-		const todo = item.parentElement;
-		todo.classList.toggle("completed");
+}
+
+function completeTodo(id) {
+	console.log("complete clicked", id);
+	todosArray[id].completed = true;
+	console.log(todosArray);
+	if (todosArray.length > 0) {
+		todoList.innerHTML = todosArray.map((todo) => {
+			const completedClass = todo.completed ? "strikethrough" : "";
+			return `
+            <div class="todo flex-between">
+            <li class="todo-item ${completedClass}">${todo.text}</li>
+            <div class="flex btn-div">
+                <button type="submit" class="complete-btn" onclick="completeTodo(${todo.id})"><i class="fas fa-check"></i></button>
+                <button type="submit" class="trash-btn" onclick="deleteTodo(${todo.id})"><i class="fas fa-trash"></i></button>
+            </div>
+        </div>`;
+		});
 	}
 }
